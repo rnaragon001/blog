fb7abe0c06016b81c402abe02eef1fb666ae2009
rnaragon001 <rnaragon001@yahoo.com>
2020-03-13 18:32:47 +0800

    index and show screens finished

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import IndexScreen from './src/screens/IndexScreen';
+import ShowScreen from './src/screens/ShowScreen';
+import CreateScreen from './src/screens/CreateScreen';
+
+import { Provider as BlogProvider} from './src/context/BlogContext';
+
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
+    Show: ShowScreen,
+    Create: CreateScreen,
+  }, 
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs'
+    }
+  }
+)
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <BlogProvider>
+      <App />
+    </BlogProvider>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
